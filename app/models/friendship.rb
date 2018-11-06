bb8dc5c54180087e1affa11afa5273e64c4bb0ea
madtab7 <madtab7@gmail.com>
2018-11-06 15:45:01 -0500

    basic user reccomendation functionality

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,4 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, :class_name => "User"
 
-  # belongs_to :friender, :class_name => "User"
-  # belongs_to :friendee, :class_name => "User"
 end
