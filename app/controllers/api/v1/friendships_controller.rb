e289eb8d23355e21ee1c99dbc3a577fad6c76ba4
madtab7 <madtab7@gmail.com>
2018-11-06 12:02:11 -0500

    added create friendships functionality

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::FriendshipsController < ApplicationController
+  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized
+
+  def index
+    @friendships = Friendship.all
+    render json: @friendships
+  end
+
+  def create
+    @friendship = Friendship.new(friendship_params)
+    if @friendship.save
+      render json: @friendship
+    end
+  end
+
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id)
+  end
+
 end
--- app/models/shelved_book.rb
+++ app/models/shelved_book.rb
@@ -2,9 +2,5 @@ class ShelvedBook < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  # def self.usersearch(user_id)
-  #   ShelvedBook.set_headers
-  #   get()
-  # end
 
 end
