530a415475089bd68e349e25cad897b898c0decc
madtab7 <madtab7@gmail.com>
2018-11-13 15:33:34 -0500

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,5 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, :source => :user
 
-  # has_many :friendships
-  #
-  # has_many :friender_friendships, class_name: "Friendship", foreign_key: "friender_id"
-  # has_many :friendee_friendships, class_name: "Friendship", foreign_key: "friendee_id"
-  #
-  # has_many :friendees, through: :friendee_friendships, class_name: "User", foreign_key: "friendee_id"
-  # has_many :frienders, through: :friender_friendships, class_name: "User", foreign_key: "friender_id"
-
 
 end
